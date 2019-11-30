3024c1eeb8734282f89f3ba1657012293cc391da
github username <github email address>
2019-11-30 17:32:02 -0500

    changed error message

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < ApplicationController
         if is_authenticated
             render json: {token: create_token(user.id)}
         else 
-            render json: {errors: ['You fucked up']}, status: 422
+            render json: {errors: ['Wrong Username or Password']}, status: 422
         end
     end
 
